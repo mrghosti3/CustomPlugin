7520ced93c9c658003fbae19af3db27ddb0dfbbb
mrghosti3 <mysteriousghst@gmail.com>
2022-04-14 14:43:54 +0300

    Initial code commit

---
+++ src/main/java/ghosti3/mcplugin/customplugin/CustomEventListener.java
@@ -0,0 +1,15 @@
+package ghosti3.mcplugin.customplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+import static org.bukkit.entity.EntityType.CREEPER;
+
+class CustomEventListener implements Listener {
+    @EventHandler
+    public void onCreeperExplode(EntityExplodeEvent evt) {
+        if (evt.getEntityType() != CREEPER) return;
+        evt.blockList().clear();
+    }
+}
